51cd5bd8f627f97d44f4c72253fb876eb009bb55
yazeedb <ybzadough@gmail.com>
2019-01-12 15:31:46 -0500

    update title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Yazeed Bzadough`,
+    description:
+      'Yazeed Bzadough is a Front-End web developer. He blogs about JavaScript on Medium.com.',
+    author: `@yazeedb`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
