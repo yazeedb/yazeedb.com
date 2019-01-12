6a9149ee93c773fb2cbb1d19d19386d9539504d5
yazeedb <ybzadough@gmail.com>
2019-01-12 16:58:11 -0500

    install dep and update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-medium',
+      options: {
+        username: '@yazeedb',
+        limit: 200,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
