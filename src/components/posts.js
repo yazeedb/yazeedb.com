4806aa584cc043405ae09b8c699a1753a4ef7337
yazeedb <ybzadough@gmail.com>
2019-03-31 15:32:35 -0400

    progress

--- src/components/posts.js
+++ src/components/posts.js
@@ -3,20 +3,37 @@ import { StaticQuery, graphql } from 'gatsby'
 
 const PostsComponent = ({ allMediumPost: { edges: posts } }) => {
   return (
-    <div className="posts">
-      <h1>My Latest Articles</h1>
-
+    <div
+      className="posts"
+      style={{
+        width: '66%',
+        margin: '0 auto',
+      }}
+    >
       <ul className="posts-list">
         {posts.map(({ node }) => (
-          <li className="post" key={node.id}>
+          <li
+            className="post"
+            key={node.id}
+            style={{
+              margin: '40px 0',
+            }}
+          >
+            {console.log(node)}
             <a
               href={`https://medium.com/@yazeedb/${node.uniqueSlug}`}
               target="_blank"
               rel="noopener noreferrer"
               alt={`${node.title}–by Yazeed Bzadough`}
+              style={{ textDecoration: 'none' }}
             >
-              {node.title}
+              <h2>{node.title}</h2>
             </a>
+            <p>
+              {node.firstPublishedAt} · {Math.round(node.virtuals.readingTime)}{' '}
+              minute read
+            </p>
+            <p>{node.content.subtitle}</p>
           </li>
         ))}
       </ul>
@@ -33,9 +50,14 @@ const Post = () => (
             node {
               id
               title
+              firstPublishedAt
               uniqueSlug
+              content {
+                subtitle
+              }
               virtuals {
                 subtitle
+                readingTime
                 previewImage {
                   imageId
                 }
