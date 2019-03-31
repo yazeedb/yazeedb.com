2f879c5910a1056f25cc2f6a17aa6daee7ba9d9e
yazeedb <ybzadough@gmail.com>
2019-03-31 15:58:30 -0400

    add dummy work page and "Coming Soon" message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => <Layout>This is my portfolio page</Layout>
+const IndexPage = () => (
+  <Layout>
+    <h1>Portfolio Coming Soon!</h1>
+    Perhaps <Link to="/blog">read my blog</Link> while you wait?
+  </Layout>
+)
 
 export default IndexPage
---
+++ src/pages/work.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const WorkPage = () => <Layout>Portfolio coming soon!</Layout>
+
+export default WorkPage
