1bde457aa0e5f1f553eec83c076abf2e83d1e585
yazeedb <ybzadough@gmail.com>
2019-04-21 19:03:25 -0400

    programmatically create pages from Markdown posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,10 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/`,
       },
     },
+    'gatsby-transformer-remark',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
