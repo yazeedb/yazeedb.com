3329aec6f013dba5783886fc6bab16d0b3996fc7
yazeedb <ybzadough@gmail.com>
2019-07-29 20:04:50 -0400

    moar meta tags

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -19,20 +19,26 @@ const Layout = ({
   siteUrl,
   coverImageUrl
 }: Props) => {
+  const fullCoverImageUrl = coverImageUrl ? siteUrl + coverImageUrl : null;
+
   return (
     <div className={styles.layout}>
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+
+        {/* OG tags */}
         <meta property="og:site_name" content={title} />
+        <meta property="og:type" content="article" />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={fullCoverImageUrl} />
+
+        {/* Twitter tags */}
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta
-          name="twitter:image"
-          content={coverImageUrl ? siteUrl + coverImageUrl : null}
-        />
+        <meta name="twitter:image" content={fullCoverImageUrl} />
         <meta name="twitter:creator" content="Yazeed Bzadough" />
       </Helmet>
       {children}
