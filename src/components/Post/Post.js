431c9a28613d73954c4c350c98c1437b718fc946
yazeedb <ybzadough@gmail.com>
2019-08-19 19:22:54 -0400

    add Post progress bar

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import Author from './Author';
 import Comments from './Comments';
@@ -13,13 +13,43 @@ type Props = {
   post: Node
 };
 
+const getScrollPercentage = () => {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  return (winScroll / height) * 100;
+};
+
 const Post = ({ post }: Props) => {
   const { html } = post;
   const { tagSlugs, slug } = post.fields;
   const { tags, title, date } = post.frontmatter;
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+  const updateScrollPercentage = () => {
+    setScrollPercentage(getScrollPercentage());
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', updateScrollPercentage);
+
+    return () => {
+      document.removeEventListener('scroll', updateScrollPercentage);
+    };
+  }, []);
+
+  console.log({ scrollPercentage });
 
   return (
     <div className={styles['post']}>
+      <div
+        className={styles['post__progress']}
+        style={{
+          width: `${scrollPercentage}%`
+        }}
+      />
       <Link className={styles['post__home-button']} to="/">
         All Articles
       </Link>
