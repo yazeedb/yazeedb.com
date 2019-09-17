2a5052966dce00ded5e15de955d43924790d64e5
yazeedb <ybzadough@gmail.com>
2019-09-16 21:38:01 -0400

    remove unused styles

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -1,4 +1,3 @@
-// @flow
 import React from 'react';
 import styles from './Contacts.module.scss';
 import TwitterIcon from '../../Icons/Twitter';
@@ -6,26 +5,28 @@ import GitHubIcon from '../../Icons/GitHub';
 import LinkedInIcon from '../../Icons/LinkedIn';
 import YouTubeIcon from '../../Icons/YouTube';
 
-type Props = {
-  contacts: {
-    [string]: string
-  }
-};
-
-const Contacts = ({ contacts }: Props) => (
+const Contacts = () => (
   <div className={styles['contacts']}>
     <ul className={styles['contacts__list']}>
-      <li>
-        <TwitterIcon />
+      <li className={styles['contacts__item']}>
+        <a href="https://twitter.com/yazeedBee">
+          <TwitterIcon />
+        </a>
       </li>
-      <li>
-        <GitHubIcon />
+      <li className={styles['contacts__item']}>
+        <a href="https://github.com/yazeedb">
+          <GitHubIcon />
+        </a>
       </li>
-      <li>
-        <LinkedInIcon />
+      <li className={styles['contacts__item']}>
+        <a href="https://www.linkedin.com/in/yazeedb/">
+          <LinkedInIcon />
+        </a>
       </li>
-      <li>
-        <YouTubeIcon />
+      <li className={styles['contacts__item']}>
+        <a href="https://www.youtube.com/channel/UC9pYepHoYW9Hr_VLDrgLhRA">
+          <YouTubeIcon />
+        </a>
       </li>
     </ul>
   </div>
