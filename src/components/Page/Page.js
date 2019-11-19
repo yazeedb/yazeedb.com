e452e72d76912a5210a851bcaf49dc616de7ec38
yazeedb <ybzadough@gmail.com>
2019-11-18 20:04:42 -0500

    remove scroll into view logic

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect } from 'react';
-import styles from './Page.module.scss';
+import React, { useRef, useEffect } from "react";
+import styles from "./Page.module.scss";
 
 type Props = {
   title?: string,
@@ -7,19 +7,11 @@ type Props = {
 };
 
 const Page = ({ title, children }: Props) => {
-  const pageRef = useRef();
-
-  useEffect(() => {
-    pageRef.current.scrollIntoView();
-  });
-
   return (
-    <div ref={pageRef} className={styles['page']}>
-      <div className={styles['page__inner']}>
-        { title && <h1 className={styles['page__title']}>{title}</h1>}
-        <div className={styles['page__body']}>
-          {children}
-        </div>
+    <div className={styles["page"]}>
+      <div className={styles["page__inner"]}>
+        {title && <h1 className={styles["page__title"]}>{title}</h1>}
+        <div className={styles["page__body"]}>{children}</div>
       </div>
     </div>
   );
